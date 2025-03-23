040f5405b6267c49f7c29ff53e5e813660fa3be2
elijahthis <emperorelijah007@gmail.com>
2025-03-23 19:27:47 +0100

    removed env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
+
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
-	env: {
-		schema: {
-			PUBLIC_BREVO_API_KEY: envField.string({
-				context: "server",
-				access: "secret",
-			}),
-			PUBLIC_BREVO_LIST_ID: envField.string({
-				context: "server",
-				access: "secret",
-			}),
-		},
-	},
 });
