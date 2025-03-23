25d6ee4b00f17073ab7d4df4b3314d24629b90ab
elijahthis <emperorelijah007@gmail.com>
2025-03-23 19:21:03 +0100

    fix: removed astro-aws-amplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
-import awsAmplify from "astro-aws-amplify";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
-	output: "server",
-	adapter: awsAmplify(),
 	env: {
 		schema: {
 			PUBLIC_BREVO_API_KEY: envField.string({
