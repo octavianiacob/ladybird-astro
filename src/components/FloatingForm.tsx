fe22cc5b9038e01f81880c387f8b1a1482f3f662
elijahthis <emperorelijah007@gmail.com>
2025-04-01 21:42:38 +0100

    form

---
+++ src/components/FloatingForm.tsx
@@ -0,0 +1,93 @@
+import { FaCheck, FaChevronRight } from "react-icons/fa6";
+import "../styles/FloatingForm.scss";
+import { useEffect, useState } from "react";
+import { LuLoaderCircle } from "react-icons/lu";
+
+const FloatingForm = () => {
+	const [email, setEmail] = useState("");
+	const [success, setSuccess] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [done, setDone] = useState(false);
+
+	const submitFunc = async () => {
+		setLoading(true);
+		try {
+			const res = await fetch(
+				"https://ladybird-subscription.fly.dev/subscribe",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ email }),
+				}
+			);
+			const data = await res.json();
+			console.log(data);
+
+			// if (data.success) {
+			setTimeout(() => {
+				setLoading(false);
+				setDone(true);
+			}, 1000);
+			// }
+		} catch (error) {
+		} finally {
+			setTimeout(() => {
+				setLoading(false);
+				setDone(false);
+				setEmail("");
+			}, 2000);
+		}
+	};
+
+	useEffect(() => {
+		const submitBtn = document.querySelector(
+			".FloatingForm__button"
+		) as HTMLButtonElement;
+
+		console.log("submitBtn", submitBtn);
+
+		// submitBtn.addEventListener("click", () => {
+		// 	console.log("jejkj");
+
+		// 	submitFunc();
+		// });
+	}, []);
+
+	return (
+		<form
+			className="FloatingForm"
+			onSubmit={(e) => {
+				e.preventDefault();
+			}}
+		>
+			<label className="FloatingForm__label">Email or Phone number</label>
+			<input
+				className=""
+				type="email"
+				name="email"
+				id=""
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<button
+				className="FloatingForm__button"
+				type="button"
+				onClick={() => {
+					submitFunc();
+				}}
+			>
+				{done ? (
+					<FaCheck />
+				) : loading ? (
+					<LuLoaderCircle className="spinner" />
+				) : (
+					<FaChevronRight />
+				)}
+			</button>
+		</form>
+	);
+};
+
+export default FloatingForm;
